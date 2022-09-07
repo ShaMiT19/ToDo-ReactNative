f7a5764e046d5102687d6e52b14721c0efddf0ae
Shamit Savant <shamitsavant1919@gmail.com>
2022-09-07 13:11:38 +0530

    Completed the Tasks.js, One error to solve: Tasks not showing in list

--- App.js
+++ App.js
@@ -1,18 +1,77 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+} from "react-native";
 import Task from "./componets/Tasks";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
+
   return (
     <View style={styles.container}>
-      {/* Task Title  */}
-      <View style={styles.taskWrapper}>
-        <Text style={styles.sectionTitle}> Today's tasks </Text>
-      </View>
-      <View style={styles.Items}>
-        <Task Text={"Task 1"} />
-        <Task Text={"Task 2"} />
-      </View>
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        {/* Task Title  */}
+        <View style={styles.taskWrapper}>
+          <Text style={styles.sectionTitle}> Today's tasks </Text>
+        </View>
+
+        {/* Write about your Items over here */}
+        <View style={styles.Items}>
+          {/* This is where the tasks will go! */}
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </ScrollView>
+
+      {/* Entry of new tasks */}
+      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -23,12 +82,42 @@ const styles = StyleSheet.create({
     backgroundColor: "#E8EAED",
   },
   taskWrapper: {
-    padding: 80,
+    paddingTop: 80,
     paddingHorizontal: 20,
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: "bold",
   },
-  Items: {},
+  Items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 20,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
 });
--- componets/Tasks.js
+++ componets/Tasks.js
@@ -1,13 +1,51 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
 const Task = (props) => {
   return (
-    <View>
-      <Text>{props.Text}</Text>
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>{props.Text}</Text>
+      </View>
+      <View style={styles.circular}></View>
     </View>
   );
 };
 
-const styles = () => {};
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    padding: 15,
+    flexDirection: "row",
+    marginBottom: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemText: {
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 4,
+  },
+});
+
 export default Task;
